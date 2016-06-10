f468ebfc4f21db871fa4f09e4ba9c891e053665b
Maria Sam <mariasam@marias-macbook-pro.local>
2016-06-09 20:38:27 -0700

    Initial Commit

---
+++ Adventure Run/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Adventure Run
+//
+//  Created by Maria Sam on 6/9/16.
+//  Copyright © 2016 Maria Sam. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
